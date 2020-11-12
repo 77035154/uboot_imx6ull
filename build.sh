1c47ed06b5f3d82cd06a71c429707732cac22afd
fanxiaobo <77035154@qq.com>
2020-11-12 16:03:15 +0800

    1.以mx6ullevk的配置文件为模板，另创一个自己的板级配置文件

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ then
 	exit
 fi
 
-make ARCH=arm CROSS_COMPILE=../gcc-4.9.4-x86_64/bin/arm-linux-gnueabihf- mx6ull_14x14_evk_emmc_defconfig
+make ARCH=arm CROSS_COMPILE=../gcc-4.9.4-x86_64/bin/arm-linux-gnueabihf- imx6ull_fxb_emmc_defconfig
 make ARCH=arm CROSS_COMPILE=../gcc-4.9.4-x86_64/bin/arm-linux-gnueabihf- -j8
